31fea67eb54e87fa9fb196ccda91da0526d3c395
wait <zhuangetwww@163.com>
2017-08-02 17:57:13 +0800

    添加单个作品模块以加入到我的作品模块中

---
+++ src/components/SubWork.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="sub-work">
+      <div class="time-container">
+        <h3 class="upload-time">{{time}}</h3>
+      </div>
+      <div class="camera-container">
+        <div class="camera">
+            <img src="../assets/works/cameraIcon.jpg" alt="1">
+        </div> 
+      </div>
+      <div class="work-img">
+           <img src="../assets/favorite/bg1.jpg" alt="2"> 
+      </div>
+  </div>
+</template>
+<script>
+
+export default {
+    data () {
+        return {
+            time:"2017-07-26 15:15:15"
+        }
+    }
+}
+</script>
+<style>
+    .sub-work {
+        width: 20.833vw;
+        position: absolute;
+        top: 25.648vh;
+    }
+    .time-container {
+        width: 100%;
+    }
+    .upload-time {
+        font-family: font757;
+        color: #f1f1f1;
+        font-size: 18px;
+        text-align: center;
+        padding: 5px auto;
+    }
+    .camera-container {
+        width: 100%;
+        height: 6.389vh;
+        margin-top: 0.4vh;
+    }
+    .camera {
+        margin-left: 8.594vw;
+        width: 3.597vw;
+        height: 100%;
+    }
+    .camera img {
+        width: 100%;
+        height: 100%;
+    }
+    .work-img {
+        width: 20.833vw;
+        height: 44.444vh;
+        position: absolute;
+        margin-top: 0;
+        /* border: 1px solid red; */
+        /* background: url(../assets/favorite/bg1.jpg) no-repeat center center; */
+    }
+    .work-img img {
+        width: 100%;
+        height: 100%;
+    } 
+
+</style>
+
--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="works">
-   <h2 class="title">{{$route.params.userName}}的作品</h2>
-   <div id="upload">
-     <img src="../assets/works/uploadIcon.png" alt="1">
+   <h2 class="title">我的作品</h2>
+    <div id="upload"> 
+     <img src="../assets/works/uploadIcon.png" alt="1">  
      <span>上传</span>
-   </div>
+    </div> 
    <date class="time"></date>
+   <sub-work class="work"></sub-work>
   </div>
 </template>
 
 <script>
 import Date from './Date'
-
+import SubWork from './SubWork'
   export default {
     name: 'works',
     data() {
@@ -19,7 +20,8 @@ import Date from './Date'
       }
     },
     components:{
-      Date
+      Date,
+      SubWork
     }
   }
 </script>
@@ -33,12 +35,47 @@ import Date from './Date'
     overflow: hidden;
   }
   .title {
-      font-family: font757;  
-      font-size: 40px;
-      color: #f1f1f1;
-      letter-spacing: 2px;
-      position: absolute;
-      top: 6.481vh;
-      left: 6.25vw;
+    font-family: font757;  
+    font-size: 40px;
+    color: #f1f1f1;
+    letter-spacing: 2px;
+    position: absolute;
+    top: 6.481vh;
+    left: 6.25vw;
+  }
+  #upload {
+    width: 6.51vw;
+    height: 4.907vh;
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    top: 6.759vh;
+    left: 20.677vw;
+    background: url("../assets/works/bt_bg2.png") no-repeat center center;
+  }
+  #upload img {
+    width: 2.76vw;
+    height: 4.907vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  #upload span {
+    font-family: font757;
+    font-size: 20px;
+    color: #f1f1f1;
+    width: 3.75vw;
+    height: 4.907vh;
+    margin: 0;
+    padding-left: 0.5vw;
+    padding-top: 1.0vh;
+    letter-spacing: 1px;
+    position: absolute;
+    top: 0;
+    left: 2.76vw;
+  }
+  .work {
+    position: absolute;
+    left: 6.25vw;
   }
 </style>
