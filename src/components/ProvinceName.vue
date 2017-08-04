653ee7b4fce7c02fd2f979b1b541f3fafc230a68
yongjianx <1402565910@qq.com>
2017-08-04 21:44:18 +0800

    搜索框动画

--- src/components/ProvinceName.vue
+++ src/components/ProvinceName.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
+      <div class="input">
         <input v-model="sText" id="searchText" type="text" placeholder="搜索内容">
         <input id="search" type="submit" value="搜索" @click="change">
-        <input :id="goUp" type="button" value="往上" @click="select(0)">
-        <input :id="goDown" type="button" value="往下" @click="select(1)">
-        
+        <input id="goUp" type="button" value="往上" @click="select(0)">
+        <input id="goDown" type="button" value="往下" @click="select(1)">
+      </div>
         <ul id="contentList">
           <li v-for="(item,index) in provinceList" :id="'id'+index" @click="nextPage(index)">
             {{item.province}}
@@ -46,6 +47,7 @@ export default {
       }else if(num==0){
         this.currentLine--;
       }
+
       var temp = document.getElementById("contentList").getElementsByTagName("li");
       for (var i = 0; i < temp.length; i++) {
           $("#contentList li").eq(i).removeClass("hightLight");
@@ -58,18 +60,22 @@ export default {
     },
 
     nextPage:function (index) {
-      //alert(index);
-      $("ul").find("li").remove();
-      var arr = area[index + 1];
-      
-      for (var i = 0; i < arr.length; i++) {
-          $("#contentList").append("<li class='cityBtn' id=cityLi" + i + ">" + arr[i].city +
-              "<span style='display:none'>" + arr[i].initial + "</span>" + "</li>");
-      }
-      $(".cityBtn").click(function() {
-          var idb = $(this).attr("id");
-          alert(idb);
+      $("#contentList").slideUp(300,function(){
+
+          $("ul").find("li").remove();
+          var arr = area[index + 1];
+          for (var i = 0; i < arr.length; i++) {
+              $("#contentList").append("<li class='cityBtn' id=cityLi" + i + ">" + arr[i].city +
+                  "<span style='display:none'>" + arr[i].initial + "</span>" + "</li>");
+          }
+          $("#contentList").slideDown();
+
+          $(".cityBtn").click(function() {
+            var idb = $(this).attr("id");
+            alert(idb);
+          });
       });
+      
     }
   }
   
@@ -78,25 +84,45 @@ export default {
 
 <style>
    body{
-    /* width: 200px; */
+    src: url("../assets/font/小米兰亭.ttf");
     font-family: "小米兰亭";
-    
   } 
-  div{
-    margin-left:100px; 
-    padding:0;
-    width: 200px;
-   
+  .input{
+    margin-left: 100px;
   }
+  
+  #searchText{
+    width: 100px;
+    height: 30px;
+  }
+
+  #search{
+    width: 40px;
+    height: 30px;
+  }
+
+  #goUp{
+    width: 40px;
+    height: 30px;
+  }
+
+  #goDown{
+    width: 40px;
+    height: 30px;
+  }
+
+
   #contentList{
+    width: 150px;
+    margin-left: 100px;
     font-size: 20pt;
     text-align: center;
     cursor: pointer;
-    background:url('../assets/search/pic1.png');
+    background:url('../assets/search/pic1.png'); 
   }
    li{
      list-style-type: none;
-     line-height: 66px;
+     /* line-height: 66px; */
      color: #f1f1f1
      
   } 
